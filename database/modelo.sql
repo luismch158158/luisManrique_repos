0301e2d75aedd2ff7fc4e619b94f4078ded728f1
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-03 23:22:00 -0500

    [Feat]: Adding doctors table

--- database/modelo.sql
+++ database/modelo.sql
@@ -12,3 +12,14 @@ CREATE TABLE medical_center (
     PRIMARY KEY (pk_medical_center)
 );
 
+CREATE TABLE doctors (
+    pk_doctor INT(11) NOT NULL AUTO_INCREMENT,
+    fk_medical_center INT(11) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    code CHAR(6) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (pk_doctor),
+    FOREIGN KEY (fk_medical_center) REFERENCES medical_center(pk_medical_center)
+);
