d6af4c69ded73a31707352619c2ba1b1b739b2a3
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-03 23:21:07 -0500

    [Feat]: Adding table medical_center

--- database/modelo.sql
+++ database/modelo.sql
@@ -3,3 +3,12 @@
 
 -- Para cargar la data desde este archivo modelo.sql
 -- cat modelo.sql | sudo mysql model_multilab
+
+CREATE TABLE medical_center (
+    pk_medical_center INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (pk_medical_center)
+);
+
