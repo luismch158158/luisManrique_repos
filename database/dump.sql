2040c33852acbc5f573c4c628f9deda0d94e643d
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-03 23:24:54 -0500

    [Feat]: Adding data for exercise 1

--- database/dump.sql
+++ database/dump.sql
@@ -1,2 +1,3 @@
-
-
+-- Añadiendo Data para el Ejercicio 1
+ INSERT INTO medical_center (name, address, created_at) VALUES ("CLINICA MONTEVIDEO", "Jr. Montevideo 448, Surco", "2022-05-27 12:07:17");
+INSERT INTO medical_center (name, address, created_at) VALUES ("CLINICA JAVIER PRADO", "Av. Javier Prado 708, San Isidro", "2022-08-27 12:07:17");
