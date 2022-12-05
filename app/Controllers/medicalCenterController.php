9a75855ae6af22ead606d05e22bba45e0aeba542
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-05 00:41:30 -0500

    [Feat]: Adding class Controller of medicalCenter to list all medical centers

---
+++ app/Controllers/medicalCenterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use Database\PDO\Connection;
+
+class medicalCenterController {
+
+    private $connection;
+
+    // Cada vez que mi clase sea instanciada se llamará al constructor
+    // El constructor asignará a la propiedad connection la instancia de la conexión a mi base de datos
+    public function __construct()
+    {
+        $this->connection = Connection::getInstance()->get_database_instance();
+    }
+
+    // Muestra una lista de centros médicos
+    public function index() {
+
+        // Usamos el metodo prepare para hacer la consulta para evitar que me hagan SQL Injection
+        // Este prepara una sentencia para su ejecución y devuelve un objeto sentencia
+        // Ayuda a prevenir inyecciones SQL eliminando la necesidad de entrecomillar manualmente los parámetros.
+
+        $stmt = $this->connection->prepare("SELECT * FROM medical_center");
+        $stmt->execute();
+
+        $finalArray = [];
+
+        while($row = $stmt->fetch(\PDO::FETCH_ASSOC))
+            array_push($finalArray, $row);
+
+        echo json_encode($finalArray);
+    }
+}
