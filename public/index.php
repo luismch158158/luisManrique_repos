d1324d2418884f3cdf8baf7b9cf3157aae6dd818
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-05 00:45:08 -0500

    [Feat]: Adding first version to GET all medical centers, need to refactor

--- public/index.php
+++ public/index.php
@@ -1,1 +1,38 @@
-<!-- This is a index.php file -->
+<?php
+
+use App\Controllers\medicalCenterController;
+
+require("vendor/autoload.php");
+
+// Definimos los recursos disponibles
+$allowedResourcesTypes = [
+    'centrosmedicos',
+    'pacientes'
+];
+
+// Validamos que el recurso este disponible
+$resourceType = $_GET['resource_type'];
+if ( !in_array( $resourceType, $allowedResourcesTypes ) ){
+    die;
+}
+
+// Se indica al cliente que lo que recibirá es un json
+header('Content-Type: application/json');
+
+// Instancio los Centros Médicos de la base de datos
+$medicalCenter_controller = new medicalCenterController;
+
+// Generamos la respuesta asumiendo que el pedido es correcto
+switch ( strtoupper($_SERVER['REQUEST_METHOD'])) {
+    case 'GET':
+        $medicalCenter_controller->index();
+        break;
+    case 'POST':
+        break;
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+}
+
+
