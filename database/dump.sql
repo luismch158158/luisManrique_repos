769eac85dfbb4a1198cd918222b036f4b1f6c282
Luis Fernando Manrique Chávez <luismanrique158158@gmail.com>
2022-12-07 00:50:59 -0500

    [Feat]: Adding how to load data dummy to medical_center and patient tables

--- database/dump.sql
+++ database/dump.sql
@@ -1,3 +1,6 @@
+-- Para cargar la data desde este archivo modelo.sql
+-- cat dump.sql | sudo mysql multilab_database
+
 -- Añadiendo Centros Médicos
 INSERT INTO medical_center (name, address, created_at) VALUES ("CLINICA MONTEVIDEO", "Jr. Montevideo 448, Surco", "2022-05-27 12:07:17");
 INSERT INTO medical_center (name, address, created_at) VALUES ("CLINICA JAVIER PRADO", "Av. Javier Prado 708, San Isidro", "2022-08-27 12:07:17");
